e15456d7d25d175e6f2fedfe25625ac5fc106a19
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-03-19 14:26:39 -0300

    Create table user-task

---
+++ app/back-end/src/database/models/UsersTasks.model.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserTasks = sequelize.define('UserTasks', {
+    userId: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true,
+      references: { model: 'Users', key: 'id' }
+    },
+    task: DataTypes.STRING,
+  },
+    {
+      timestamps: false,
+      tableName: 'user_tasks',
+      underscored: true,
+    });
+
+    UserTasks.associate = (models) => {
+      UserTasks.belongsTo(models.Users,{ 
+        as: 'user',
+        foreignKey: 'userId',
+      })
+    }
+
+  return UserTasks;
+};

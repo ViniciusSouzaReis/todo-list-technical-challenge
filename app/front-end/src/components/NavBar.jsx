e86a9142d271b137985cf92161bfe3d67cf106d8
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-03-19 22:14:48 -0300

    feat: Nav bar created

--- app/front-end/src/components/NavBar.jsx
+++ app/front-end/src/components/NavBar.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { readStorage, removeKey } from '../utils/localStorage';
+
+import '../styles/NavBar.css';
 
 function NavBar() {
+  const [name, setName] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = readStorage();
+
+    if (!user.name) navigate('/login');
+
+    setName(user.name)
+  },[navigate]);
+
+  const logoutBtn = () => {
+    removeKey();
+    navigate('/login');
+  }
+
   return (
-    <p>Barra de navegacao</p>
+    <header>
+      <h2>{name}</h2>
+      <button 
+        type="button"
+        onClick={ () => logoutBtn() }
+      >
+        Sair
+      </button>
+    </header>
+    
   );
 };
 

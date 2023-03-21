d2efe92b2b0a8971417f1ed994cd7f26bd0d2544
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-03-20 22:26:32 -0300

    feat: create JWT middleware

--- app/back-end/src/api/routes/userTasks.route.js
+++ app/back-end/src/api/routes/userTasks.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const validateJWT = require('../JWT/validateJWT');
 
 const route = express.Router();
 
 const { getIdTask, create, deleteTask, updateTask } = require('../controller/userTasks.controller');
 
-route.get('/tasks/:id', getIdTask);
-route.post('/register/:id', create);
-route.delete('/delete/:id/:data', deleteTask);
-route.patch('/update/:id', updateTask);
+route.get('/tasks/:id', validateJWT, getIdTask);
+route.post('/register/:id', validateJWT, create);
+route.delete('/delete/:id/:data', validateJWT, deleteTask);
+route.patch('/update/:id', validateJWT, updateTask);
 
 module.exports = route;
